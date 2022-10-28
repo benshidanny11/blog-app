0cfa8284b3b0798d13adc1f90ca72eb6531f2f0f
benshidanny11 <benshidanny11@gmail.com>
2022-10-28 11:37:59 +0200

    Add forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,32 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new
+    @user = current_user
+    post = Post.new
+    respond_to do |format|
+      format.html { render :new, locals: { post: } }
+    end
+  end
+
+  def create
+    @user = current_user
+    post = Post.new(params.require(:post).permit(:text, :title))
+    puts post.user_id = @user.id
+    respond_to do |format|
+      format.html do
+        if post.save
+          post.increment_counter
+          flash[:success] = 'Post created!'
+          redirect_to user_posts_path(@user)
+        else
+          flash.now[:error] = 'Post not created!'
+          render :new, locals: { post: }
+        end
+      end
+    end
+  end
+
   def get_user_name(user_id)
     User.find(user_id).name
   end
